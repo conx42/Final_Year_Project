f3ee517665d7657b055d018f151702cf6d2eefb9
conx42 <97751032+conx42@users.noreply.github.com>
2023-11-30 22:51:05 +0530

    7th_sem_final_year_project_done

---
+++ custom_ip.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int i=9,j=5,k=0,l=100;
+    if(i>100){
+        printf("\nHi");
+        if (j<9)
+        {
+            printf("\nPoss");
+            if (k<0)
+            {
+                printf("\nTest");
+            }
+            
+        }
+    }
+    
+    if (l<101)
+    {
+        printf("\nTom");
+    }
+
+}
